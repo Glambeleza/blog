91fa8a886d776a54b730af0a434554c196340e8f
valdisneinilo <valdisneinilo@gmail.com>
2023-09-28 14:46:20 -0300

    header e footer, criação de svg logo

--- app/components/header/index.tsx
+++ app/components/header/index.tsx
@@ -17,7 +17,41 @@ export default function HeaderLayout() {
         </ul>
       </nav>
 
-      <h1 className={styles.logo}>Glambeleza</h1>
+      {/* <h1 className={styles.logo}>Glambeleza</h1> */}
+      <h1 className={styles.logo}>
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="200"
+          height="70"
+          viewBox="0 0 200 70"
+        >
+          <defs>
+            <linearGradient
+              id="text-gradient"
+              x1="0%"
+              y1="0%"
+              x2="100%"
+              y2="0%"
+            >
+              <stop offset="0%" stop-color="#eaa59f" />
+              <stop offset="100%" stop-color="#6f233f" />
+            </linearGradient>
+          </defs>
+          <text
+            x="50%"
+            y="50%"
+            font-family="Tangerine"
+            font-size="50"
+            fontWeight={700}
+            text-anchor="middle"
+            dominant-baseline="middle"
+            fill="url(#text-gradient)"
+          >
+            Glambeleza
+          </text>
+        </svg>
+      </h1>
+
       <div className={styles.menuMobile}>
         <GiHamburgerMenu />
       </div>
--- app/contato/page.tsx
+++ app/contato/page.tsx
@@ -1,5 +1,11 @@
 import { Metadata } from "next";
-
+import {
+  BiLogoGmail,
+  BiLogoInstagramAlt,
+  BiLogoTiktok,
+  BiLogoYoutube,
+} from "react-icons/bi";
+import styles from "./page.module.css";
 export const metadata: Metadata = {
   title: "Contato Glambeleza",
   description: "Contato Glambeleza",
@@ -7,8 +13,47 @@ export const metadata: Metadata = {
 
 export default function ContatoPage() {
   return (
-    <main>
-      <h1>Contato Glambeleza</h1>;
-    </main>
+    <section className={styles.container}>
+      <h1 className={styles.title}>Contato</h1>
+      <h2 className={styles.subtitle}>Nós estamos aqui para te ajudar</h2>
+      <div className={styles.containerInfoForm}>
+        <div className={styles.containerInfo}>
+          <h3 className={styles.titleInfo}>Fale conosco</h3>
+          <nav>
+            <ul className={styles.listInfo}>
+              <li className={styles.itemInfo}>
+                <BiLogoInstagramAlt /> instagram.com/glambeleza
+              </li>
+              <li className={styles.itemInfo}>
+                <BiLogoTiktok /> tiktok.com/glambeleza
+              </li>
+              <li className={styles.itemInfo}>
+                <BiLogoYoutube /> youtube.com/glambeleza
+              </li>
+              <li className={styles.itemInfo}>
+                <BiLogoGmail /> contato@glambeleza.com.br
+              </li>
+            </ul>
+          </nav>
+        </div>
+
+        <div className={styles.containerForm}>
+          <form action="" className={styles.form}>
+            <input type="text" name="nome" placeholder="Digite seu nome" />
+            <input
+              type="email"
+              name="email"
+              placeholder="Digite o email que você mais usa"
+            />
+            <textarea
+              name="mensagem"
+              placeholder="Digite uma mensagem 💌"
+              rows={6}
+            />
+            <button type="submit">Enviar</button>
+          </form>
+        </div>
+      </div>
+    </section>
   );
 }
