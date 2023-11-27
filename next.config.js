64ccce6d779cb7985307fd7de67c7571c46a1f3c
valdisneinilo <valdisneinilo@gmail.com>
2023-11-27 14:42:51 -0300

    criação do workflou cypress

--- next.config.js
+++ next.config.js
@@ -1,11 +1,31 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+
   images: {
-    domains: [
-      "localhost",
-      "github.com",
-      "stablo-template.vercel.app",
-      "encrypted-tbn0.gstatic.com",
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "github.com",
+      },
+      {
+        protocol: "http",
+        hostname: "localhost",
+      },
+      {
+        protocol: "https",
+        hostname: "stablo-template.vercel.app",
+      },
+      {
+        protocol: "https",
+        hostname: "encrypted-tbn0.gstatic.com",
+      },
     ],
   },
 };
